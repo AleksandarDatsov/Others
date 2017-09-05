f8a1c9075b10e44663500e97fd85ffafd1fff690
AleksandarDatsov <a.datsov@gmail>
2017-09-05 17:49:18 +0300

    Delete old project

--- 4_PhoneBook/src/IMap.java
+++
@@ -1,8 +0,0 @@
-
-public interface IMap {
-	public String addRecord(String name, String phNumber, int outgoingCalls);
-	public String deleteRecord(String name);
-	public String viewPhoneNumber(String name);
-	public String printArr();
-	public String printTopFiveWithMostOutgoingCalls();
-}
